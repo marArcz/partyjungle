43cbb7a9bf1ef33cc771769f20a04f6501b3ead5
marArcz <mar19arcz@gmail.com>
2023-02-13 23:55:39 +0800

    updates

--- admin/products.php
+++ admin/products.php
@@ -32,18 +32,18 @@
                     </div>
                     <div class="card rounded-4 border-0 shadow-sm">
                         <div class="card-body">
-                            <?php 
-                                // category
-                                if(!isset($_GET['category'])){
-                                    $query = mysqli_query($con,"SELECT * FROM categories ORDER BY id ASC LIMIT 1");
-                                }else{
-                                    $category_id = $_GET['category'];
-                                    $query = mysqli_query($con,"SELECT * FROM categories WHERE id = $category_id");
-                                }
+                            <?php
+                            // category
+                            if (!isset($_GET['category'])) {
+                                $query = mysqli_query($con, "SELECT * FROM categories ORDER BY id ASC LIMIT 1");
+                            } else {
+                                $category_id = $_GET['category'];
+                                $query = mysqli_query($con, "SELECT * FROM categories WHERE id = $category_id");
+                            }
 
-                                $category = $query->fetch_assoc();
+                            $category = $query->fetch_assoc();
                             ?>
-                            
+
 
                             <div class="text-start">
                                 <!-- <a href="#category-modal" data-bs-toggle="modal" class="link-secondary fw-bold text-decoration-none">
@@ -51,9 +51,33 @@
                                     <?php echo $category['category_name'] ?>
                                     <i class="bx bx-filter"></i>
                                 </a> -->
+                                <div class="dropdown">
+                                    <!-- <button class="btn btn-secondary dropdown-toggle" type="button"
+                                        data-bs-toggle="dropdown" aria-expanded="false">
+                                        Dropdown button
+                                    </button> -->
+                                    <a role="button" aria-expanded="false" data-bs-toggle="dropdown" class="link-secondary fw-bold text-decoration-none">
+                                        <i class="bx bx-chevron-down"></i>
+                                        <?php echo $category['category_name'] ?>
+                                        <i class="bx bx-filter"></i>
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <?php
+                                        $query = mysqli_query($con, "SELECT * FROM categories");
+                                        while ($row = $query->fetch_assoc()) {
+                                        ?>
+                                            <li><a class="dropdown-item" href="products.php?category=<?php echo $row['id'] ?>"><?php echo $row['category_name'] ?></a></li>
+                                        <?php
+                                        }
+                                        ?>
+                                        <!-- <li><a class="dropdown-item" href="#">Another action</a></li>
+                                        <li><a class="dropdown-item" href="#">Something else here</a></li> -->
+                                    </ul>
+                                </div>
                             </div>
                             <div class="alert alert-info mb-4 mt-2 py-2">
-                                <small>Showing products from <strong><?php echo $category['category_name'] ?></strong> category.</small>
+                                <small>Showing products from <strong><?php echo $category['category_name'] ?></strong>
+                                    category.</small>
                             </div>
                             <table class="table" id="table">
                                 <thead>
@@ -64,24 +88,24 @@
                                     <th>Action</th>
                                 </thead>
                                 <tbody>
-                                    <?php 
-                                        $category_id = $category['id'];
-                                        $query = mysqli_query($con,"SELECT * FROM products WHERE category_id = $category_id");
-                                        while($row = $query->fetch_assoc()){
-                                            ?>
-                                               <tr>
-                                                <td><?php echo $row['product_name'] ?></td>
-                                                <td><?php echo $category['category_name'] ?></td>
-                                                <td><?php echo $row['price'] ?></td>
-                                                <td><?php echo $row['stocks'] ?></td>
-                                                <td>
-                                                    <a href="#manage-modal" data-bs-toggle="modal" class="link-dark">
-                                                        <i class="bx bx-eye"></i>
-                                                    </a>
-                                                </td>
-                                               </tr> 
-                                            <?php
-                                        }
+                                    <?php
+                                    $category_id = $category['id'];
+                                    $query = mysqli_query($con, "SELECT * FROM products WHERE category_id = $category_id");
+                                    while ($row = $query->fetch_assoc()) {
+                                    ?>
+                                        <tr>
+                                            <td><?php echo $row['product_name'] ?></td>
+                                            <td><?php echo $category['category_name'] ?></td>
+                                            <td><?php echo $row['price'] ?></td>
+                                            <td><?php echo $row['stocks'] ?></td>
+                                            <td>
+                                                <a href="#manage-modal" data-bs-toggle="modal" class="link-dark">
+                                                    <i class="bx bx-eye"></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    <?php
+                                    }
                                     ?>
                                 </tbody>
                             </table>
@@ -95,7 +119,7 @@
     <?php include './includes/scripts.php' ?>
     <?php include './includes/alerts.php' ?>
     <script>
-        
+
     </script>
 </body>
 
